5dc3ded0e46e1f1bbc003e8483bee5ceccbfaeec
GNUDimarik <dmitry.adjiev@gmail.com>
2023-07-27 13:15:18 +0600

    Added additional compiler flags for warnings

--- cmake/common_c_cxx_flags.cmake
+++ cmake/common_c_cxx_flags.cmake
@@ -1,2 +1,2 @@
-set(ENV{COMMON_CXX_FLAGS} "-I$ENV{MACONDO_INCLUDE}/include -std=gnu++20 -O3 -Wall -Wextra -Werror -nostdlib -ffreestanding -fno-exceptions -fno-rtti -mcpu=cortex-a72+nosimd")
-set(ENV{COMMON_C_FLAGS} "-I$ENV{MACONDO_INCLUDE}/include -std=gnu18 -O3 -Wall -Wextra -Werror -nostdlib -ffreestanding -mcpu=cortex-a72+nosimd")
+set(ENV{COMMON_CXX_FLAGS} "-I$ENV{MACONDO_INCLUDE}/include -std=gnu++20 -O3 -Wall -Wextra -Wabi -Werror -nostdlib -ffreestanding -fno-exceptions -fno-rtti -mcpu=cortex-a72+nosimd")
+set(ENV{COMMON_C_FLAGS} "-I$ENV{MACONDO_INCLUDE}/include -std=gnu18 -O3 -Wall -Wextra -Wabi -Werror -nostdlib -ffreestanding -mcpu=cortex-a72+nosimd")
